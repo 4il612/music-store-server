3f29e6136697007c0356b067ede54c55cc616922
Nikita Shilin <shilin.nick@yandex.ru>
2022-08-11 14:17:33 +0300

    feat: .env | db | schemas
    Created .env file with PORT and PASSWORD variables, connectd in app.module
    Made a db connetion
    Created base schemas of Track and Comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TrackModule } from './track/track.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports:[TrackModule]
+    imports:[
+        ConfigModule.forRoot({ envFilePath: '.env' }),
+        TrackModule,
+        MongooseModule.forRoot(`mongodb+srv://nikita:${process.env.PASSWORD}@cluster0.4q0msoa.mongodb.net/?retryWrites=true&w=majority`)
+    ]
 })
 export class AppModule {}
