1dc648a02c30047444db07c22932de2705419d63
Nikita Shilin <shilin.nick@yandex.ru>
2022-08-11 14:37:06 +0300

    feat: Track create
    Created Track DTO for POST request
    Added POST request of create functional

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { CreateTrackDto } from "./dto/createTrack.dto";
+import { TrackService } from "./track.service";
 
 @Controller('tracks')
 export class TrackController{
-    create(){
+    constructor(
+        private trackService: TrackService
+    ){}
 
+    @Post()
+    create(@Body() dto: CreateTrackDto){
+        return this.trackService.create(dto)
     }
 
     @Get()
